99530bd47e9afa793ee6262bec03cd112441a120
Miquel Sabaté Solà <mikisabate@gmail.com>
2025-04-02 22:46:40 +0200

    config: Reduce the velocities when airborne
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- config/values/player.s
+++ config/values/player.s
@@ -2,25 +2,25 @@
 ;; DO NOT MODIFY this file directly: check config/values.yml instead.
 
 .ifdef PAL
-    GRAVITY = $30
+    GRAVITY = $23
     BLAST_OFF = $F3
-    THRUST = $D8
+    THRUST = $E9
     FLY_LEFT = $D8
     FLY_RIGHT = $24
     WALK_LEFT = $ED
-    WALK_RIGHT = $0E
+    WALK_RIGHT = $12
     BOUNCE_LEFT = $DE
     BOUNCE_RIGHT = $1D
     REDUCE_FULL_SPEED = $13
     REDUCE_MID_SPEED = $0A
 .else
-    GRAVITY = $28
+    GRAVITY = $1C
     BLAST_OFF = $F8
-    THRUST = $E2
+    THRUST = $E0
     FLY_LEFT = $E2
     FLY_RIGHT = $1D
     WALK_LEFT = $F3
-    WALK_RIGHT = $0C
+    WALK_RIGHT = $0E
     BOUNCE_LEFT = $E7
     BOUNCE_RIGHT = $18
     REDUCE_FULL_SPEED = $10
