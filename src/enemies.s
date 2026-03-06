mssola/jetpac.nes

commit 7c493ba3f338ab09b24a26a1b4a8ad3d1060597a

Improve a bit the performance on enemy death
Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

src/enemies.s
@@ -597,16 +597,12 @@
     .proc bite_the_dust
         dec Enemies::zp_enemies_pool_size
 
-        ldx Enemies::zp_pool_index
-
         ;; Invalidate this enemy.
         lda #$FF
+        ldx Enemies::zp_pool_index
         sta Enemies::zp_enemies_pool_base, x
         sta Enemies::zp_current_tiles, x
         sta Enemies::zp_current_tiles + 1, x
-
-        stx Globals::zp_tmp0
-        sty Globals::zp_tmp1
 
         ;; Create an explosion for this enemy.
         lda Enemies::zp_enemies_pool_base + 1, x
@@ -615,12 +611,10 @@
         sta Globals::zp_arg3
         jsr Explosions::create
 
-        ldx Globals::zp_tmp0
-        ldy Globals::zp_tmp1
-
         ;; The 'extra' value is now a "revive counter". Whenever it times out
         ;; this enemy will be eligible to go back to life.
         lda #REVIVE_COUNTER
+        ldx Enemies::zp_pool_index
         sta Enemies::zp_enemies_pool_base + 3, x
 
         rts

commit f3737201eda72e51730a0762be98598fee08d86b

Fix a cycling bug when shooting carelessly
When shooting like crazy and killing random enemies, after a while a
cycling bug would appear which would make enemies to turn in weird
directions, die unexpectedly, and other weird shenanigans.

This comes back to commit 7c493ba3f338 ("Improve a bit the performance
on enemy death"), which removed the saving/restoring of the value on the
'y' register. That being said, this register was actually being needed
at least by Bullets::update(), so in certain situations this register
might have a bad value (e.g. via the Explosions::create() call).

Fixes: 7c493ba3f338 ("Improve a bit the performance on enemy death")

Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

src/enemies.s
@@ -613,7 +613,16 @@
     .endproc
 
     ;; The enemy has been set to dust, remove it.
+    ;;
+    ;; NOTE: the 'x' register is modified, the 'y' register is _preserved_.
     .proc bite_the_dust
+        ;; This function might be called by loops which abuse index
+        ;; registers. Luckily that's not the case for the 'x' register, but at
+        ;; least the loop on Bullets::update() does heavy use of the 'y'
+        ;; register. Preserve it now on the stack.
+        tya
+        pha
+
         ;; Invalidate this enemy.
         lda #$FF
         ldx Enemies::zp_pool_index
@@ -633,6 +642,10 @@
         lda #REVIVE_COUNTER
         ldx Enemies::zp_pool_index
         sta Enemies::zp_enemies_pool_base + 3, x
+
+        ;; Restore back the value for the 'y' register.
+        pla
+        tay
 
         rts
     .endproc