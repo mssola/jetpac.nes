mssola/jetpac.nes

commit bc99212539f6da721ab50f4cd5e3ab0b4e76827e

Toggle a "Paused" message on top of the screen
Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

src/driver.s
@@ -44,6 +44,11 @@
     ;; of RAM left.
     zp_moved_out = $38
 
+    ;; Whether the pause message on the HUD has to be toggled. Like
+    ;; 'zp_moved_out', maybe a waste of resource to dedicate a full byte for
+    ;; this, but we have plenty of RAM left.
+    zp_pause_toggle = $39
+
     ;; Switch from the title screen to the main screen. Note that this function
     ;; is to be called with the PPU disabled. If that's not the case, then it
     ;; will set the proper values to disable it on the next `nmi` call and set
@@ -148,11 +153,11 @@
         jsr Enemies::init
         jsr Explosions::init
 
-        ;; Initialize pause timer and whether sprites have been moved out of the
-        ;; screen.
+        ;; Initialize pause timer and some boolean values.
         lda #0
         sta zp_pause_timer
         sta Driver::zp_moved_out
+        sta Driver::zp_pause_toggle
 
         ;; Initialize variables for sprite cycling.
         sta zp_next_bullet_cycle
@@ -182,6 +187,10 @@
         ;; Let's reset the timer.
         lda #PAUSE_TIMER_VALUE
         sta zp_pause_timer
+
+        ;; Toggle the message on the HUD.
+        lda #1
+        sta Driver::zp_pause_toggle
 
         ;; Pause vs unpause.
         lda #%00001000
@@ -519,4 +528,50 @@
             rts
         .endproc
     .endif
+
+    ;; Toggle the "Paused" message from the (not quite) HUD.
+    ;;
+    ;; NOTE: only call this function from NMI code.
+    .proc hud_toggle_pause
+        lda #%00001000
+        and Globals::zp_flags
+        bne @paused
+
+        ;; Clear out the "Paused" message.
+        bit PPU::m_status
+        lda #$28
+        sta PPU::m_address
+        lda #$8D
+        sta PPU::m_address
+        lda #0
+        sta PPU::m_data
+        sta PPU::m_data
+        sta PPU::m_data
+        sta PPU::m_data
+        sta PPU::m_data
+        sta PPU::m_data
+        rts
+
+    @paused:
+        bit PPU::m_status
+        lda #$28
+        sta PPU::m_address
+        lda #$8D
+        sta PPU::m_address
+
+        lda #$2A                ; P
+        sta PPU::m_data
+        lda #$1B                ; A
+        sta PPU::m_data
+        lda #$2F                ; U
+        sta PPU::m_data
+        lda #$2D                ; S
+        sta PPU::m_data
+        lda #$1F                ; E
+        sta PPU::m_data
+        lda #$1E                ; D
+        sta PPU::m_data
+
+        rts
+    .endproc
 .endscope

src/interrupts.s
@@ -24,6 +24,12 @@
     lda #$02
     sta OAM::m_dma
 
+    ;; Toggle pause message from the HUD.
+    lda Driver::zp_pause_toggle
+    beq @increase_rand
+    jsr Driver::hud_toggle_pause
+
+@increase_rand:
     ;; Increase the random seed.
     inc Prng::zp_rand
 

commit e8f39dc4a7cc19fb97e24ec0819ccd964cbd325f

Initialize 'zp_pause_toggle' before NMI enablement
Otherwise we might randomly get weird scenarios like the title screen
not showing up properly.

Fixes: bc99212539f6 ("Toggle a "Paused" message on top of the screen")

Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

src/driver.s
@@ -49,6 +49,15 @@
     ;; this, but we have plenty of RAM left.
     zp_pause_toggle = $39
 
+    ;; Initialization routine that is to be called before enabling NMIs back for
+    ;; the first time.
+    .proc init_before_nmi
+        lda #0
+        sta Driver::zp_pause_toggle
+
+        rts
+    .endproc
+
     ;; Switch from the title screen to the main screen. Note that this function
     ;; is to be called with the PPU disabled. If that's not the case, then it
     ;; will set the proper values to disable it on the next `nmi` call and set
@@ -157,7 +166,6 @@
         lda #0
         sta zp_pause_timer
         sta Driver::zp_moved_out
-        sta Driver::zp_pause_toggle
 
         ;; Initialize variables for sprite cycling.
         sta zp_next_bullet_cycle

src/jetpac.s
@@ -121,7 +121,8 @@
     sta PPU::zp_control
 
     ;; Initialize other global variables which the rest of the game assume to
-    ;; have zero as their initial values.
+    ;; have zero as their initial values. Note that it's important to have these
+    ;; variables defined before
     sta Globals::zp_flags
     sta Joypad::zp_buttons1
     sta Joypad::zp_buttons2
@@ -145,6 +146,10 @@
 
     ;; Initialize some variables from the "Game Over" side of the game.
     jsr Over::init
+
+    ;; Initialize variables from the game's driver that need to be set before
+    ;; NMIs start ticking.
+    jsr Driver::init_before_nmi
 
     ;; Initialize some PAL-specific constants.
     .ifdef PAL