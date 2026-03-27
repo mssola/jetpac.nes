mssola/jetpac.nes

commit 63ef73de3bdd698ab250407354e24a6de7c9ef20

Update scores on screen
Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

src/bullets.s
@@ -297,8 +297,9 @@
         jsr Enemies::collides
         beq @next_enemy_collision
 
-        ;; Yes! Kill the enemy and break the loop.
+        ;; Yes! Kill the enemy, keep the score and break the loop.
         jsr Enemies::bite_the_dust
+        ADD_ENEMY_SCORE
         jmp @save_bullet_move
 
     @next_enemy_collision:

src/items.s
@@ -521,6 +521,14 @@
         lsr
         sta Items::zp_current_tiles + 1, x
 
+        ;; Account for this on the player's score.
+        ;;
+        ;; NOTE: this is in opposition as to how it was handled in the original
+        ;; game where the score was accounted on part/tank pickup, not
+        ;; dropping. I find this more reliable and easier to code, and in the
+        ;; end it's the same.
+        ADD_PART_FUEL_SCORE
+
         jmp @next
 
         ;;;
@@ -966,7 +974,8 @@
         lda #$FF
         sta Items::zp_pool_base, x
 
-        ;; TODO: score
+        ;; Account for this on the player's score.
+        ADD_ITEM_SCORE
 
         rts
     .endproc

src/score.s
@@ -118,9 +118,79 @@
 
         BCD_JUST_CARRY
 
-        ;; TODO: set a flag about updating the score on the HUD.
+        ;;;
+        ;; And set the 'score' flag, signaling a need of updating the score.
+
+        lda Globals::zp_extra_flags
+        ora #$80
+        sta Globals::zp_extra_flags
 
         rts
+    .endproc
+
+    ;; Update the score for both players. This might be a bit too much on single
+    ;; player mode but we have to show both scores on the title screen
+    ;; anyways. Hence, since we have all the time in the world anyways, we
+    ;; update both and avoid branching and stuff.
+    .proc nmi_update_scores
+        ;; The 'y' register will contain the right high byte for the PPU
+        ;; address. This is needed because scores are to be displayed on both
+        ;; the title and game screens.
+        lda PPU::zp_control
+        and #$02
+        tax
+        ldy hi_ppu_address, x
+
+        ;; Now we just put the numbers. The 'x' index has to go "backwards", and
+        ;; taking into account that both players live on the same buffer. The
+        ;; tile ID is basically the integer value + $10, which is the position
+        ;; of the '0' character on our tile set.
+
+        bit PPU::m_status
+        sty PPU::m_address
+        lda #$62
+        sta PPU::m_address
+
+        clc
+        ldx #(PLAYERS_BUFF_SIZE - 2)
+    @player1_loop:
+        lda Score::m_players, x
+        adc #$10
+        sta PPU::m_data
+
+        dex
+        dex
+        cpx #$FE
+        bne @player1_loop
+
+        ;; And the same for the second player.
+
+        bit PPU::m_status
+        sty PPU::m_address
+        lda #$78
+        sta PPU::m_address
+
+        clc
+        ldx #(PLAYERS_BUFF_SIZE - 1)
+    @player2_loop:
+        lda Score::m_players, x
+        adc #$10
+        sta PPU::m_data
+
+        dex
+        dex
+        cpx #$FF
+        bne @player2_loop
+
+        ;; Disable the 'score' flag.
+        lda Globals::zp_extra_flags
+        and #$3F
+        sta Globals::zp_extra_flags
+
+        rts
+
+    hi_ppu_address:
+        .byte $20, $00, $28, $00
     .endproc
 .endscope
 

include/globals.s
@@ -56,4 +56,15 @@
     ;; |   1 | player's 1 state | 0: over; 1: alive        |
     ;; |   0 | active           | 0: player 1; 1: player 2 |
     zp_multiplayer = $26
+
+    ;; Extra bitmap that was needed beyond the ones that we already have. Yeah,
+    ;; I know, bad planning from my side, but now it's a bit complex to untangle
+    ;; variables like 'Globals::zp_flags'.
+    ;;
+    ;; | Bit | Short name       | Meaning                          |
+    ;; |-----+------------------+----------------------------------|
+    ;; |   7 | score            | The score has been updated.      |
+    ;; |   6 | high             | The high score has been updated. |
+    ;; | 5-0 | -                | Unused.                          |
+    zp_extra_flags = $27
 .endscope

src/driver.s
@@ -138,6 +138,10 @@
         and #%11111011
         sta Globals::zp_flags
 
+        ;; And the scores should be updated on the game screen as well.
+        lda #$80
+        sta Globals::zp_extra_flags
+
         rts
     .endproc
 

src/interrupts.s
@@ -27,6 +27,12 @@
     lda #$02
     sta OAM::m_dma
 
+    ;; Should scores be updated?
+    bit Globals::zp_extra_flags
+    bpl @check_pause
+    jsr Score::nmi_update_scores
+
+@check_pause:
     ;; Toggle pause message from the HUD.
     bit Driver::zp_flags
     bvc @increase_rand

src/jetpac.s
@@ -107,7 +107,7 @@
     bpl @vblankwait2
 
     ;; NOTE: palettes are not initialized here as it's going to be one of the
-    ;; first things done on `main` code.
+    ;; first things done in main().
 
     __fallthrough__ main
 .endproc
@@ -123,6 +123,12 @@
     sta Score::m_hi + 5
 
 @init:
+    ;; Force the scores to appear.
+    lda #$80
+    sta Globals::zp_extra_flags
+
+    ;; TODO: update high score (see zp_extra_flags)
+
     ;; Disable the PPU and zero out variables which shadow PPU registers.
     lda #0
     sta PPU::m_mask

commit a2411d86a504adbc027a93c8bddb1ba0e0f447fd

Fix overwrite issues when adding scores
The add_to_player() function relied on parameters to be passed on the
'Globals::zp_tmpX' variables, but that's just plain stupid. First,
because these memory addresses are not meant for arguments; but most
importantly, because callers actually needed some of these values before
calling this function. Hence, everything went into chaos and sprites
began to act wrongly in all sorts of random ways.

Moreover, some of the callers also needed a guarantee on either the 'x'
or the 'y' registers. Since this wasn't always feasible, we needed to
store/load these registers' values case by case.

All in all, now add_to_player() accepts only one argument via the 'y'
register (which callers already knew it was going to be messed
with). The value on 'y' has to be set via some helper constants.

As a cherry on top, this not only fixes a nasty bug, but it also makes
things more performant and with less code.

Fixes: 63ef73de3bdd ("Update scores on screen")
Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

src/bullets.s
@@ -299,7 +299,9 @@
 
         ;; Yes! Kill the enemy, keep the score and break the loop.
         jsr Enemies::bite_the_dust
+        sty Globals::zp_tmp2
         ADD_ENEMY_SCORE
+        ldy Globals::zp_tmp2
         jmp @save_bullet_move
 
     @next_enemy_collision:

src/items.s
@@ -527,7 +527,9 @@
         ;; game where the score was accounted on part/tank pickup, not
         ;; dropping. I find this more reliable and easier to code, and in the
         ;; end it's the same.
+        stx Globals::zp_tmp2
         ADD_PART_FUEL_SCORE
+        ldx Globals::zp_tmp2
 
         jmp @next
 
@@ -946,8 +948,6 @@
 
     ;; Collect an item as indexed by 'zp_pool_index'. This function assumes that
     ;; the item is already valid.
-    ;;
-    ;; NOTE: the 'y' register is preserved.
     .proc collect
         ldx Items::zp_pool_index
 
@@ -975,7 +975,9 @@
         sta Items::zp_pool_base, x
 
         ;; Account for this on the player's score.
+        stx Globals::zp_tmp2
         ADD_ITEM_SCORE
+        ldx Globals::zp_tmp2
 
         rts
     .endproc

src/score.s
@@ -1,10 +1,10 @@
-;; Clear the carry and add ADDR to the indexed digit on 'Score::m_players'. If
-;; the result is larger or equal than 10, then 0 is stored and the carry flag is
-;; set. Otherwise the carry flag is cleared.
+;; Clear the carry and add ADDR, y to the indexed digit on
+;; 'Score::m_players'. If the result is larger or equal than 10, then 0 is
+;; stored and the carry flag is set. Otherwise the carry flag is cleared.
 .macro BCD_ADD ADDR
     lda Score::m_players, x
     clc
-    adc ADDR
+    adc ADDR, y
     cmp #10
     bcc :+
     sec
@@ -13,12 +13,12 @@
     sta Score::m_players, x
 .endmacro
 
-;; Add ADDR to the indexed digit on 'Score::m_players' _with_ carry. If the
+;; Add ADDR, y to the indexed digit on 'Score::m_players' _with_ carry. If the
 ;; result is larger or equal than 10, then 0 is stored and the carry flag is
 ;; set. Otherwise the carry flag is cleared.
 .macro BCD_ADDC ADDR
     lda Score::m_players, x
-    adc ADDR
+    adc ADDR, y
     clc
     cmp #10
     bcc :+
@@ -65,6 +65,12 @@
     ;; The high score for this session.
     m_hi = $30C           ; asan:reserve $06
 
+    ;; Indeces for the 'additions' address. Relevant only when calling
+    ;; add_to_player_y().
+    ADD_ENEMY_IDX = 0
+    ADD_PART_FUEL_IDX = 3
+    ADD_ITEM_IDX = 6
+
     ;; Initialize the scores for both players.
     .proc init_players_scores
         lda #0
@@ -79,10 +85,10 @@
         rts
     .endproc
 
-    ;; Add to the current player's score the number stored in
-    ;; 'Globals::zp_tmp{0,1,2}', where 'Globals::zp_tmp0' has the least
-    ;; significant number.
-    .proc add_to_player
+    ;; Add to the current player's score the number stored in "additions, y". As
+    ;; a caller you don't need to know the exact format of this data, so set to
+    ;; 'y' one of the 'ADD_*_IDX' constants from up above.
+    .proc add_to_player_y
         ;; See 'Score::m_players' on why this is the way to select the current
         ;; player's score.
         lda Globals::zp_multiplayer
@@ -91,17 +97,17 @@
 
         ;;;
         ;; The first three digits are the product of adding the contents of
-        ;; 'Globals::zp_tmp{0,1,2}'.
+        ;; 'additions, y'.
 
-        BCD_ADD Globals::zp_tmp0
-        inx
-        inx
-
-        BCD_ADDC Globals::zp_tmp1
+        BCD_ADD additions
         inx
         inx
 
-        BCD_ADDC Globals::zp_tmp2
+        BCD_ADDC additions + 1
+        inx
+        inx
+
+        BCD_ADDC additions + 2
         inx
         inx
 
@@ -126,6 +132,18 @@
         sta Globals::zp_extra_flags
 
         rts
+
+        ;; Defines the values by which the score can be incremented. They are
+        ;; three bytes per object in little-endian format.
+    additions:
+        ;; Enemy
+        .byte $05, $02, $00
+
+        ;; Part / fuel tank
+        .byte $00, $00, $01
+
+        ;; Item
+        .byte $00, $05, $02
     .endproc
 
     ;; Save the score of either of the two players if any of them are higher
@@ -292,34 +310,19 @@
 
 ;; Add the score for a dead enemy to the current player's score.
 .macro ADD_ENEMY_SCORE
-    lda #5
-    sta Globals::zp_tmp0
-    lda #2
-    sta Globals::zp_tmp1
-    lda #0
-    sta Globals::zp_tmp2
-    jsr Score::add_to_player
+    ldy #Score::ADD_ENEMY_IDX
+    jsr Score::add_to_player_y
 .endmacro
 
 ;; Add the score for grabbing a shuttle part / fuel tank to the current player's
 ;; score.
 .macro ADD_PART_FUEL_SCORE
-    lda #0
-    sta Globals::zp_tmp0
-    lda #0
-    sta Globals::zp_tmp1
-    lda #1
-    sta Globals::zp_tmp2
-    jsr Score::add_to_player
+    ldy #Score::ADD_PART_FUEL_IDX
+    jsr Score::add_to_player_y
 .endmacro
 
 ;; Add the score for grabbing an item to the current player's score.
 .macro ADD_ITEM_SCORE
-    lda #0
-    sta Globals::zp_tmp0
-    lda #5
-    sta Globals::zp_tmp1
-    lda #2
-    sta Globals::zp_tmp2
-    jsr Score::add_to_player
+    ldy #Score::ADD_ITEM_IDX
+    jsr Score::add_to_player_y
 .endmacro